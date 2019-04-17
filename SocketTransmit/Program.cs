3856348d55ab26ca5a4141c03a0bbe3aaf236eb3
Iyui <314754801@qq.com>
2019-04-17 13:33:34 +0800

    添加项目文件。

---
+++ SocketTransmit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketTransmit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
