d9ea4c40cb21a016673314abdc89e69de5492a5f
Iyui <314754801@qq.com>
2019-04-17 16:11:04 +0800

    init

--- Transmit.Server/Server.cs
+++ Transmit.Server/Server.cs
@@ -1,24 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
+using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Threading;
 using System.IO;
 using System.IO.Ports;
-
-namespace SocketTransmit
+namespace Transmit.Server
 {
-    public class SocketTransmit
+    public partial class Server : Form
     {
+        public Server()
+        {
+            InitializeComponent();
+        }
+
         public int port
         {
-            get;set;
+            get; set;
         }
 
         public int backlog
@@ -26,12 +32,6 @@ namespace SocketTransmit
             get; set;
         }
 
-
-        public SocketTransmit(int port,int backlog)
-        {
-            this.port = port;
-            this.backlog = backlog;
-        }
         #region 变量
         //用于通信的Socket
         Socket socketSend;
@@ -42,6 +42,13 @@ namespace SocketTransmit
         Thread AcceptSocketThread;
         //接收客户端发送消息的线程
         Thread threadReceive;
+        static HashSet<Socket> hsSocket = new HashSet<Socket>();
+
+        static HashSet<Socket> hsPCSocket = new HashSet<Socket>();
+        static HashSet<Socket> hsModuleSocket = new HashSet<Socket>();
+
+        static Hashtable htPCSocket = new Hashtable();
+        static Hashtable htModuleSocket = new Hashtable();
 
         #endregion
         public void Listern()
@@ -61,11 +68,12 @@ namespace SocketTransmit
             //Thread send = new Thread(Send);
             //send.Start();
             ////创建线程
-            //AcceptSocketThread = new Thread(new ParameterizedThreadStart(StartListen));
-            //AcceptSocketThread.IsBackground = true;
-            //AcceptSocketThread.Start(socketWatch);
+            AcceptSocketThread = new Thread(new ParameterizedThreadStart(StartListen))
+            {
+                IsBackground = true
+            };
+            AcceptSocketThread.Start(socketWatch);
         }
-
         /// <summary>
         /// 等待客户端的连接，并且创建与之通信用的Socket
         /// </summary>
@@ -78,6 +86,9 @@ namespace SocketTransmit
                 //等待客户端的连接，并且创建一个用于通信的Socket
                 socketSend = socketWatch.Accept();
                 //获取远程主机的ip地址和端口号
+
+                hsSocket.Add(socketSend);
+
                 string strIp = socketSend.RemoteEndPoint.ToString();
                 string strMsg = "远程主机：" + socketSend.RemoteEndPoint + "连接成功";
                 //使用回调
@@ -85,7 +96,6 @@ namespace SocketTransmit
                 threadReceive = new Thread(new ParameterizedThreadStart(Receive));
                 threadReceive.IsBackground = true;
                 threadReceive.Start(socketSend);
-
                 Thread.Sleep(1);
             }
         }
@@ -110,12 +120,70 @@ namespace SocketTransmit
                     {
                         break;
                     }
+                    else
+                    {
+                        DisposeSignal(buffer, socketSend);
+                    }
                 }
                 catch
                 {
 
                 }
                 Thread.Sleep(1);
+            }
+        }
+
+        private void Send(Socket socketSend,byte[] bytes)
+        {
+            ((Socket)htMoudlePC[socketSend]).Send(bytes);
+        }
+
+        static Hashtable htMoudlePC = new Hashtable();
+        /// <summary>
+        /// 连接时判断是模块还是PC,并进行配对
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="socketSend"></param>
+        private void DisposeSignal(byte[] buffer, Socket socketSend)
+        {
+            var s = "";
+            foreach (var c in buffer)
+                s += c.ToString("X2");
+            var message = s.Split(':');
+            if (message[0] == "shgarden")
+            {
+                hsPCSocket.Add(socketSend);
+                htPCSocket.Add(message[1], socketSend);
+                if (htPCSocket[message[1]] != socketSend)
+                {
+                    htPCSocket.Remove(message[1]);
+                    htPCSocket.Add(message[1], socketSend);
+
+                    htMoudlePC.Remove(socketSend);
+                }
+                if(htModuleSocket.ContainsKey(message[1]))
+                {
+                    htMoudlePC.Add(htModuleSocket[message[1]], socketSend);
+                    htMoudlePC.Add(socketSend,htModuleSocket[message[1]]);
+                }
+
+            }
+            else if (message[0] == "nedraghs")
+            {
+                hsModuleSocket.Add(socketSend);
+                htModuleSocket.Add(message[1], socketSend);
+                if(htModuleSocket[message[1]] != socketSend)
+                {
+                    htModuleSocket.Remove(message[1]);
+                    htModuleSocket.Add(message[1], socketSend);
+
+                    htMoudlePC.Remove(socketSend);
+                }
+                if (htPCSocket.ContainsKey(message[1]))
+                {
+                    htMoudlePC.Add(htPCSocket[message[1]], socketSend);
+                    htMoudlePC.Add(socketSend, htPCSocket[message[1]]);
+                }
             }
         }
 
